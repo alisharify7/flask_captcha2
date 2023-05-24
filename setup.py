2b41bae81d6bc8cda617f929a62f533a762d8e4d
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-05-24 08:25:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
-from flask_captcha2.auther import __version__
+from flask_captcha2.auther import __version__, __auther__,__NAME__, __short_description__
 
 with open("./readme.md") as f:
     long_description = f.read()
 
 setup(
-    name="flask_captcha2",
+    name=__NAME__ ,
     version=__version__,
-    description="an light and simple flask extension for integrate google recaptcha with Flask Apps",
+    description=__short_description__,
     packages=find_packages(),
     author_email="alisharifyofficial@gmail.com",
-    author="Ali Sharify",
+    author=__auther__,
     url="https://github.com/alisharify7/flask_captcha2",
     long_description=long_description,
     classifiers=[
+        "Programming Language :: Python :: 3.11"
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
     ],
     license="MIT",
