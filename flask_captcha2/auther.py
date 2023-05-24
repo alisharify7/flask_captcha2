18717b790b71071567189915809df0c74580f10d
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-05-24 08:24:21 +0330

    Update auther.py

--- flask_captcha2/auther.py
+++ flask_captcha2/auther.py
@@ -4,6 +4,6 @@ __version__ = "2.0.0"
 __author__ = "ali sharify"
 __copyright__ = "ali sharify - 2023"
 __license__ = "MIT"
-
+__short_description__ = "an light and simple flask extension for integrate google recaptcha with Flask Apps"
 
 
