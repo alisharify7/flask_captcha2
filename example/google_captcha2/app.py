94890bc03d0899c0c7bc6151ba272caefe91d38c
ali sharifi <alisharifyofficial@gmail.com>
2024-01-31 22:32:17 +0330

    update example apps

--- example/google_captcha2/app.py
+++ example/google_captcha2/app.py
@@ -2,19 +2,26 @@
 example :
     captcha version 2
 """
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    raise ValueError("dotenv library is not installed, install it with `pip install python-dotenv` ")
 
+import os
 from flask import Flask, render_template
 from flask_captcha2 import FlaskCaptcha2
 
+
 app = Flask(__name__)
 
+load_dotenv()
+
 app.config.update({
-    "RECAPTCHA_PRIVATE_KEY": '',
-    "RECAPTCHA_PUBLIC_KEY": '',
+    "RECAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V2", ""),
+    "RECAPTCHA_PUBLIC_KEY": os.environ.get("PUBLIC_KEY_V2", ""),
     'RECAPTCHA_ENABLED': True,  # captcha enable status
     "RECAPTCHA_LOG": True,
     "RECAPTCHA_LANGUAGE": "en"
-
 })
 
 captcha = FlaskCaptcha2()
--- example/google_captcha3/app.py
+++ example/google_captcha3/app.py
@@ -3,23 +3,32 @@ example :
     captcha version 3
 """
 
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    raise ValueError("dotenv library is not installed, install it with `pip install python-dotenv` ")
+
+import os
 from flask import Flask, render_template
 from flask_captcha2 import FlaskCaptcha3
 
+
 app = Flask(__name__)
 
+load_dotenv()
+
 app.config.update({
-    "RECAPTCHA_PRIVATE_KEY": 'secret key',
-    "RECAPTCHA_PUBLIC_KEY":'public key',
-    'RECAPTCHA_ENABLED':True, # captcha enable status
-    "RECAPTCHA_SCORE": 0.5, # 
+    "RECAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
+    "RECAPTCHA_PUBLIC_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
+    'RECAPTCHA_ENABLED': True,  # captcha enable status
+    "RECAPTCHA_SCORE": 0.5,  #
     "RECAPTCHA_LOG": True
 
 })
 
 captcha = FlaskCaptcha3()
 captcha.init_app(app=app)
-# or 
+# or
 # captcha = FlaskCaptcha3(app=app)
 
 
@@ -34,7 +43,6 @@ def index_post():
 @app.get("/")
 def index_get():
     return render_template("login.html")
-
 
 
 if __name__ == "__main__":
