005e15c3baae7f15718e40279914427f476026d6
ali sharifi <alisharifyofficial@gmail.com>
2024-02-01 14:01:51 +0330

    :8ball:  add base testing

---
+++ tests/captchaV2/test_google_captcha2.py
@@ -0,0 +1,42 @@
+import os
+
+
+def test_captcha_config_is_correct(googlecaptcha2, app):
+    """This test ensure that Flask-Captcha can read and apply all config correctly
+    from app.config object to google-captcha-v2 object"""
+    assert googlecaptcha2.PRIVATE_KEY == app.config.get("RECAPTCHA_PRIVATE_KEY")
+    assert googlecaptcha2.PUBLIC_KEY == app.config.get("RECAPTCHA_PUBLIC_KEY")
+    assert googlecaptcha2.ENABLED == app.config.get("RECAPTCHA_ENABLED")
+    assert googlecaptcha2.CAPTCHA_LOG == app.config.get("RECAPTCHA_LOG")
+    assert googlecaptcha2.LANGUAGE == app.config.get("RECAPTCHA_LANGUAGE")
+    assert googlecaptcha2.TABINDEX == app.config.get("RECAPTCHA_TABINDEX")
+    assert googlecaptcha2.SIZE == app.config.get("RECAPTCHA_SIZE")
+    assert googlecaptcha2.TYPE == app.config.get("RECAPTCHA_TYPE")
+    assert googlecaptcha2.THEME == app.config.get("RECAPTCHA_THEME")
+
+
+
+def test_refresh_captcha_config(googlecaptcha2, app):
+    """Test refresh method work properly"""
+    # change the app config while the app running <captcha config in app.config>
+    app.config["RECAPTCHA_LOG"] = False
+    assert googlecaptcha2.CAPTCHA_LOG != app.config["RECAPTCHA_LOG"]
+
+    googlecaptcha2.refresh_conf(app) # refresh new config from flask App
+    assert googlecaptcha2.CAPTCHA_LOG == app.config["RECAPTCHA_LOG"]
+
+
+def test_load_captcha_in_html():
+    ...
+
+
+
+def test_captcha_valid_key():
+    ...
+
+
+
+def test_captcha_invalid_key():
+    ...
+
+
---
+++ tests/conftest.py
@@ -0,0 +1,70 @@
+import os
+
+
+
+import pytest
+from dotenv import load_dotenv
+
+
+from flask import Flask
+from flask_captcha2 import FlaskCaptcha
+
+
+
+
+@pytest.fixture()
+def app():
+    """Main Flask Application fixture"""
+    app = Flask(__name__)
+    app.config.update({
+        "TESTING": True,
+        # 'DEBUG': True
+    })
+    yield app
+
+@pytest.fixture()
+def googlecaptcha2(app):
+    """flask-captcha2 Google Captcha v2 object"""
+    app.config.from_mapping(
+        {
+            "RECAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V2", "sample key"),
+            "RECAPTCHA_PUBLIC_KEY": os.environ.get("PUBLIC_KEY_V2", "sample key"),
+            'RECAPTCHA_ENABLED': True,
+            "RECAPTCHA_LOG": True,
+            "RECAPTCHA_LANGUAGE": "en",
+            "RECAPTCHA_THEME": "light",
+            "RECAPTCHA_SIZE": "normal",
+            "RECAPTCHA_TABINDEX": 0,
+            "RECAPTCHA_TYPE": "image"
+        }
+    )
+    Master_captcha = FlaskCaptcha(app=app)
+    captcha = Master_captcha.getGoogleCaptcha2('flask-captcha-v2')
+    yield captcha
+
+@pytest.fixture()
+def googlecaptcha3(app):
+    """flask-captcha2 Google Captcha v3 object"""
+    app.config.from_mapping(
+        {
+            "RECAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
+            "RECAPTCHA_PUBLIC_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
+            'RECAPTCHA_ENABLED': True,  # captcha enable status
+            "RECAPTCHA_SCORE": 0.5,  #
+            "RECAPTCHA_LOG": True,
+
+        }
+    )
+    Master_captcha = FlaskCaptcha(app=app)
+    captcha = Master_captcha.getGoogleCaptcha2('flask-captcha-v2')
+
+    yield captcha
+
+
+@pytest.fixture()
+def client(app):
+    """Simple client for testing flask application"""
+    yield app.test_client()
+
+
+
