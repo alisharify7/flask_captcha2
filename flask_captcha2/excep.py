89327e23ed42bd60179c0e17b18f0e2c436e30c1
devbarsamco <ali.sharify@barsam.co>
2024-07-21 08:41:27 +0330

    add doc string for exception classes

--- flask_captcha2/excep.py
+++ flask_captcha2/excep.py
@@ -1,8 +1,17 @@
-class NotFlaskApp(Exception):
-    """Custom exception class"""
+class BaseFlaskCaptchaException(Exception):
+    """Base FlaskCaptcha Exception """
+    ...
+
+class NotFlaskApp(BaseFlaskCaptchaException):
+    """Custom exception class
+        this exception raise when an invalid or wrong
+        app passed in __init__ or init_app function
+    """
     ...
 
 
-class CaptchaNameNotExists(Exception):
-    """Custom exception class"""
+class CaptchaNameNotExists(BaseFlaskCaptchaException):
+    """Custom exception class
+        this exception raise when render_captcha func get wrong model name
+    """
     ...
