5b3d4a703aff0745d20081a07b016c8930bb6b53
ali sharifi <alisharifyofficial@gmail.com>
2024-08-22 16:43:56 +0330

    fix doc strings

--- flask_captcha2/Logger.py
+++ flask_captcha2/Logger.py
@@ -3,25 +3,27 @@ import sys
 
 
 def get_logger(
-    LogLevel: int, CaptchaName: str = "Flask-Captcha"
+    log_level: int, captcha_name: str = "Flask-Captcha"
 ) -> logging.Logger:
-    """create a custom stdout Logger
+    """create a custom stdout Logger with given level and name
 
-    Args:
-        CaptchaName:string: name of the logger
+    :param captcha_name: name of the logger
+    :type captcha_name: str
 
-    Returns:
-        logging.Logger
+    :param log_level: logging level
+    :type log_level: int
 
+    :return: logging.Logger
+    :rtype: logging.Logger
     """
-    logLevel = LogLevel or logging.DEBUG
+    log_level = log_level or logging.DEBUG
     logformat = logging.Formatter(
-        f"[{CaptchaName}" + "- %(levelname)s] [%(asctime)s] - %(message)s"
+        f"[{captcha_name}" + "- %(levelname)s] [%(asctime)s] - %(message)s"
     )
-    logger = logging.getLogger(CaptchaName)
-    logger.setLevel(logLevel)
+    logger = logging.getLogger(captcha_name)
+    logger.setLevel(log_level)
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logLevel)
+    handler.setLevel(log_level)
     handler.setFormatter(logformat)
     logger.addHandler(handler)
     return logger
