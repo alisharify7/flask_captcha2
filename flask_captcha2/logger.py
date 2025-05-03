57446d6f32b7737d5692d196fc2821ffc61e158b
Ali Sharify <alisharifyofficial@gmail.com>
2025-05-03 08:31:42 +0330

    :sparkles: improve logger method

---
+++ flask_captcha2/logger.py
@@ -0,0 +1,67 @@
+"""
+ * flask_captcha2 OSS
+ * author: github.com/alisharify7
+ * email: alisharifyofficial@gmail.com
+ * license: see LICENSE for more details.
+ * Copyright (c) 2023 - ali sharifi
+ * https://github.com/alisharify7/flask_captcha2
+"""
+import logging
+import sys
+from typing import Optional
+
+
+def get_logger(
+        log_level: Optional[int] = None,
+        logger_name: str = "Flask-Captcha",
+        log_format: Optional[str] = None,
+        handlers: Optional[list[logging.Handler]] = None
+) -> logging.Logger:
+    """Create and configure a custom Logger with given level, name, and formatting.
+
+    Args:
+        log_level: Logging level (e.g., logging.DEBUG, logging.INFO). Defaults to logging.DEBUG.
+        logger_name: Name of the logger. Defaults to "Flask-Captcha".
+        log_format: Custom log format string. If None, uses a default format.
+        handlers: List of custom handlers to add. If None, adds a stdout StreamHandler.
+
+    Returns:
+        Configured Logger instance.
+
+    Example:
+        >>> logger = get_logger(logging.INFO)
+        >>> logger.info("Test message")
+    """
+    # Set defaults
+    log_level = logging.DEBUG if log_level is None else log_level
+
+    # Create default format if none provided
+    if log_format is None:
+        log_format = (
+            f"[{logger_name} - %(levelname)s] "
+            "[%(asctime)s] [%(filename)s:%(lineno)d] - %(message)s"
+        )
+
+    formatter = logging.Formatter(log_format)
+
+    # Get or create logger
+    logger = logging.getLogger(logger_name)
+
+    # Avoid adding handlers multiple times in case of repeated calls
+    if not logger.handlers:
+        # Use provided handlers or create default stdout handler
+        if handlers is None:
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(log_level)
+            handler.setFormatter(formatter)
+            handlers = [handler]
+
+        for handler in handlers:
+            logger.addHandler(handler)
+
+    logger.setLevel(log_level)
+
+    # Prevent propagation to root logger if desired
+    logger.propagate = False
+
+    return logger
