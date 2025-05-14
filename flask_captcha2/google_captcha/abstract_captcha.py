31a86d1cff6f440465e80545f0b1df0ac7b6cec4
ali sharifi <alisharifyofficial@gmail.com>
2025-05-14 17:04:48 +0330

    add type annotation to abstract class for google capcha

--- flask_captcha2/google_captcha/abstract_captcha.py
+++ flask_captcha2/google_captcha/abstract_captcha.py
@@ -1,27 +1,29 @@
 from abc import ABC, abstractmethod
+from markupsafe import Markup
+
 
 class GoogleCaptchaInterface(ABC):
     """
     All Google Captcha related classes must implement 
     all methods in this abstract base class.
     """
-
-    @abstractmethod
-    def set_config(self):
-        """Set the configuration for the captcha."""
-        pass
-
-    @abstractmethod
-    def is_verify(self):
-        """Verify the captcha response."""
-        pass
-
-    @abstractmethod
-    def render_widget(self):
-        """Render the captcha widget in the UI."""
-        pass
-
     @abstractmethod
     def init_app(self):
         """Initialize the captcha with the application context."""
         pass
+
+    @abstractmethod
+    def set_config(self, conf: dict) -> None:
+        """Set the configuration for the captcha."""
+        pass
+
+    @abstractmethod
+    def is_verify(self) -> bool:
+        """Verify the captcha response."""
+        pass
+
+    @abstractmethod
+    def render_widget(self) -> Markup:
+        """Render the captcha widget in the UI."""
+        pass
+
