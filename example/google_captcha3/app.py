3d812d8ffbe2421d4f82d1e0fede9858be031dfc
Ali Sharify <alisharifyofficial@gmail.com>
2025-05-19 16:32:40 +0330

    add another captcha version3 to examples

--- example/google_captcha3/app.py
+++ example/google_captcha3/app.py
@@ -18,18 +18,28 @@ app = Flask(__name__)
 
 load_dotenv()
 
-app.config.update(
-    {
+app.config.update()
+captcha_manager = FlaskCaptcha(app=app)  # app is required
+google_captcha_v3 = captcha_manager.generate_google_captcha_v3(
+    namespace="captcha3",
+    conf={
         "captcha_private_key": os.environ.get("captcha_private_key", ""),
         "captcha_public_key": os.environ.get("captcha_public_key", ""),
         "captcha_enabled": True,  # captcha status <True, False> True: Production , False: development
         "captcha_score": 0.5,  # google captcha version3 works with scores
         "captcha_log": True,  # show captcha requests and logs in terminal > stdout
-    }
-)
-captcha_manager = FlaskCaptcha(app=app)  # app is required
-google_captcha_v3 = captcha_manager.generate_google_captcha_v3(
-    namespace="captcha3"
+    },
+)  # generate google captcha v3 object
+
+another_google_captcha_v3 = captcha_manager.generate_google_captcha_v3(
+    namespace="another_captcha3",
+    conf={
+        "captcha_private_key": os.environ.get("captcha_private_key", ""),
+        "captcha_public_key": os.environ.get("captcha_public_key", ""),
+        "captcha_enabled": True,  # captcha status <True, False> True: Production , False: development
+        "captcha_score": 0.5,  # google captcha version3 works with scores
+        "captcha_log": True,  # show captcha requests and logs in terminal > stdout
+    },
 )  # generate google captcha v3 object
 
 
