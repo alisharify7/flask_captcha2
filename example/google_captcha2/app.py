8b009129e165ba3eb63c445e3f1cad3b2a4c7b26
Ali Sharify <alisharifyofficial@gmail.com>
2025-07-08 15:46:28 +0330

    update examples for captcha google version 2

--- example/google_captcha2/app.py
+++ example/google_captcha2/app.py
@@ -18,29 +18,52 @@ app = Flask(__name__)
 
 load_dotenv()
 
-app.config.update(
-    {
+hidden_conf =     {
         "captcha_private_key": os.environ.get("captcha_private_key", ""),
         "captcha_public_key": os.environ.get("captcha_public_key", ""),
         "captcha_enabled": True,  # captcha status <True, False> True: Production , False: development
         "captcha_log": True,  # show captcha logs in console
         "captcha_language": "en",  # captcha language
+        "captcha_type": "invisible",
+        "captcha_size": "invisible",
     }
-)
+
+simple_conf =     {
+    "captcha_private_key": os.environ.get("captcha_private_key", ""),
+    "captcha_public_key": os.environ.get("captcha_public_key", ""),
+    "captcha_enabled": True,  # captcha status <True, False> True: Production , False: development
+    "captcha_log": True,  # show captcha logs in console
+    "captcha_language": "en",  # captcha language
+}
 
 captcha_manager = FlaskCaptcha(app=app)  # app is required
-google_captcha_version3 = captcha_manager.generate_google_captcha_v2(
-    namespace="captcha2"
+
+google_v2_hidden = captcha_manager.generate_google_captcha_v2(
+    namespace="hidden",
+    conf=hidden_conf
+)
+
+google_v2_simple = captcha_manager.generate_google_captcha_v2(
+    namespace="simple",
+    conf=simple_conf
 )  # created a Google captcha v2 object
 
-
-@app.post("/")
-def index_post():
+@app.post("/hidden")
+def hidden():
     """index view: post method"""
-    if google_captcha_version3.is_verify():
-        return "captcha is ok"
+    if google_v2_hidden.is_verify():
+        return "hidden captcha is ok"
     else:
-        return "invalid captcha"
+        return "invalid hidden captcha"
+
+
+@app.post("/simple")
+def simple():
+    """index view: post method"""
+    if google_v2_simple.is_verify():
+        return "simple 'im not robot' captcha is ok"
+    else:
+        return "invalid 'im not robot' captcha"
 
 
 @app.get("/")
