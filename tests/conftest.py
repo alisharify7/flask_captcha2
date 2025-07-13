ed6356c2a85ebc10ac36a70bebe41e29db3e0cf9
Ali Sharify <alisharifyofficial@gmail.com>
2025-07-13 16:25:18 +0330

    update pytest conf test

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,41 +23,41 @@ def googlecaptcha2_simple(app):
     """flask-captcha2 Google Captcha v2 object"""
     app.config.from_mapping(
         {
-            "CAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V2", "sample key"),
-            "CAPTCHA_PUBLIC_KEY": os.environ.get("PUBLIC_KEY_V2", "sample key"),
-            "CAPTCHA_ENABLED": True,
-            "CAPTCHA_LOG": False,
-            "CAPTCHA_LANGUAGE": "en",
-            "CAPTCHA_THEME": "light",
-            "CAPTCHA_SIZE": "normal",
-            "CAPTCHA_TABINDEX": 0,
-            "CAPTCHA_TYPE": "image",
+            "captcha_private_key": os.environ.get("PRIVATE_KEY_V2", "sample key"),
+            "captcha_public_key": os.environ.get("PUBLIC_KEY_V2", "sample key"),
+            "captcha_enabled": True,
+            "captcha_log": False,
+            "captcha_language": "en",
+            "captcha_theme": "light",
+            "captcha_size": "normal",
+            "captcha_tabindex": 0,
+            "captcha_type": "image",
         }
     )
-    MainCaptcha = FlaskCaptcha(app=app)
-    captcha = MainCaptcha.get_google_captcha_v2("flask-captcha-v2")
-    yield captcha
+    captcha_manager = FlaskCaptcha(app=app)
+    captcha2 = captcha_manager.generate_google_captcha_v2(namespace="flask-captcha-v2")
+    yield captcha2
 
 
 @pytest.fixture()
-def googlecaptcha2_hidden_badge(app):
+def googlecaptcha2_hidden(app):
     """flask-captcha2 Google Captcha v2 object"""
     app.config.from_mapping(
         {
-            "CAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V2", "sample key"),
-            "CAPTCHA_PUBLIC_KEY": os.environ.get("PUBLIC_KEY_V2", "sample key"),
-            "CAPTCHA_ENABLED": True,
-            "CAPTCHA_LOG": False,
-            "CAPTCHA_LANGUAGE": "en",
-            "CAPTCHA_THEME": "light",
-            "CAPTCHA_SIZE": "normal",
-            "CAPTCHA_TABINDEX": 0,
-            "CAPTCHA_TYPE": "image",
+            "captcha_private_key": os.environ.get("PRIVATE_KEY_V2", "sample key"),
+            "captcha_public_key": os.environ.get("PUBLIC_KEY_V2", "sample key"),
+            "captcha_enabled": True,
+            "captcha_log": False,
+            "captcha_language": "en",
+            "captcha_theme": "light",
+            "captcha_size": "invisible",
+            "captcha_tabindex": 0,
+            "captcha_type": "invisible",
         }
     )
-    MainCaptcha = FlaskCaptcha(app=app)
-    captcha = MainCaptcha.get_google_captcha_v2("flask-captcha-v2")
-    yield captcha
+    captcha_manager = FlaskCaptcha(app=app)
+    captcha2 = captcha_manager.generate_google_captcha_v2(namespace="flask-captcha-v2")
+    yield captcha2
 
 
 @pytest.fixture()
@@ -65,16 +65,15 @@ def googlecaptcha3(app):
     """flask-captcha2 Google Captcha v3 object"""
     app.config.from_mapping(
         {
-            "CAPTCHA_PRIVATE_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
-            "CAPTCHA_PUBLIC_KEY": os.environ.get("PRIVATE_KEY_V3", ""),
-            "CAPTCHA_ENABLED": True,  # captcha enable status
-            "CAPTCHA_SCORE": 0.5,  #
-            "CAPTCHA_LOG": False,
+            "captcha_private_key": os.environ.get("PRIVATE_KEY_V3", "test"),
+            "captcha_public_key": os.environ.get("PRIVATE_KEY_V3", "test"),
+            "captcha_enabled": True,  # captcha enable status
+            "captcha_score": 0.5,
+            "captcha_log": False,
         }
     )
     MainCaptcha = FlaskCaptcha(app=app)
     captcha = MainCaptcha.get_google_captcha_v3("flask-captcha-v3")
-
     yield captcha
 
 
@@ -94,5 +93,33 @@ def captcha3_template_conf():
         "inline_css": 'background-color:"red"',
         "id": "id-of-submit-form",
         "css_class": "class-of-submit-form",
+    }
+    yield conf
+
+
+@pytest.fixture()
+def captcha2_simple_template_conf():
+    """captcha version3 render_captcha config"""
+    conf = {
+        "namespace": "simple",
+        "inline_css": "margin: 10px;",
+        "css_class": "custom_css_class",
+        "id": "captcha-v2",
+        "dataset": "data-ok='true'",
+    }
+    yield conf
+
+
+@pytest.fixture()
+def captcha2_hidden_template_conf():
+    """captcha version3 render_captcha config"""
+    conf = {
+        "namespace": "hidden",
+        "inline_css": "margin: 10px;",
+        "css_class": "form-control my-2 bg-success text-white fs-4",
+        "id": "captcha-v2",
+        "dataset": "data-ok='true'",
+        "parent_form_id": "hidden_parent",
+        "button_text": "Submit",
     }
     yield conf
